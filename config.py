cfff43abc7e8e0141819b65da900186eaef3875f
ValentinaLV <tina.lysenok@gmail.com>
2019-12-29 21:40:53 +0200

    removed config.py from git

--- config.py
+++
@@ -1,5 +0,0 @@
-class Configuration(object):
-    DEBUG = True
-    SECRET_KEY = 'ewewvdcsvfdsgs213240+++(((FWSFDF'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Tinka140792@localhost:5432/postgres'
